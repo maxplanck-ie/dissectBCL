b4623a69ee5bd9f20e4bd6a8c644ede625df2fd4
WardDeb <ward@deboutte.be>
2021-11-05 16:09:04 +0100

    executable

---
+++ build/scripts-3.9/dissect.py
@@ -0,0 +1,13 @@
+#!python
+from dissectBCL import preFQ
+from rich import print, inspect
+
+def main():
+    config = preFQ.getConf()
+    a = preFQ.getNewFlowCell(config)
+    a = preFQ.parseRunInfo( a )
+    a = preFQ.parseSS( a )
+    inspect( a )
+
+if __name__ == "__main__":
+    main()
--- setup.py
+++ setup.py
@@ -6,11 +6,13 @@ setuptools.setup(
     author = 'WardDeb',
     author_email = 'deboutte@ie-freiburg.mpg.de',
     description = "MPI-IE's demux pipeline. Continuation of 'TheWhoTheWhatTheHuh' from DpRyan. ",
-    package_dir = {"": "dissectBCL"},
+    scripts=['bin/dissect.py'],
+    packages = ["dissectBCL"],
     python_requires = "==3.9.2",
     install_requires = [
-        'rich==10.12.0',
+        'flake8==3.9.2',
         'matplotlib==3.4.3',
-        'flake8==3.9.2'
+        'pandas==1.3.4',
+        'rich==10.12.0'
     ]
 )
