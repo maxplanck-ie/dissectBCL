70bc26f714541bb622e0b0c613c2102ac994fd4c
WardDeb <ward@deboutte.be>
2021-11-08 12:29:51 +0100

    flake8 dissect.py

--- bin/dissect.py
+++ bin/dissect.py
@@ -3,14 +3,16 @@ from dissectBCL import preFQ
 from dissectBCL import FQ
 from rich import print, inspect
 
+
 def main():
     config = preFQ.getConf()
     a = preFQ.getNewFlowCell(config)
     if a:
-        a = preFQ.parseRunInfo( a )
-        a = preFQ.parseSS( a )
-        inspect( a )
-        FQ.dirCreator( a )
+        print("New flowcell found.")
+        a = preFQ.parseRunInfo(a)
+        a = preFQ.parseSS(a)
+        inspect(a)
+        FQ.dirCreator(a)
 
 
 if __name__ == "__main__":
