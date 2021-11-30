bee8f1844b5c922431f739f589aa88b751ab2ed3
WardDeb <ward@deboutte.be>
2021-11-30 16:00:31 +0100

    demux flake

--- dissectBCL/demux.py
+++ dissectBCL/demux.py
@@ -1,14 +1,11 @@
 from dissectBCL.fakeNews import log
-from dissectBCL.misc import joinLis, hamming, lenMask, bclConvPipeLogger
+from dissectBCL.misc import joinLis, hamming, lenMask
+from dissectBCL.misc import bclConvPipeLogger, P5Seriesret
 from itertools import combinations
 import os
-import pandas as pd
 from subprocess import Popen, PIPE
+import sys
 
-
-'''
-Cycle, masking, mismatching and sampleSheetClass changes.
-'''
 
 def misMatcher(P7s, P5s):
     """
@@ -37,9 +34,14 @@ def misMatcher(P7s, P5s):
             mmDic['BarcodeMismatchesIndex2'] = 0
     return mmDic
 
+
 def detMask(seqRecipe, sampleSheetDF, outputFolder):
     """
-    Determine the actual masking based on the runInfo stats ('seqRecipe') and the information in the sampleSheet / parkour
+    Determine the actual masking.
+    Based on:
+        - seqRecipe (RunInfo.xml)
+        - sampleSheet
+        - parkour info
     """
     log.info("determine masking for {}".format(outputFolder))
     mask = []
@@ -57,10 +59,14 @@ def detMask(seqRecipe, sampleSheetDF, outputFolder):
         recipeP5 = seqRecipe['Index2'][1]
     if 'Read2' in seqRecipe:
         PE = True
-    ## Capture both cases with either NuGEN Ovation Solo (P7 UMI), or scATAC (P5 UMI)
+    # Capture NuGEN Ovation Solo or scATAC
     if 'indexType' in list(sampleSheetDF.columns):
-        ####################### NUGEN OVATION SOLO #######################
-        if any(sampleSheetDF['indexType'].str.contains("NuGEN Ovation SoLo RNA-Seq System")):
+        # Nugen Ovation SOLO
+        if any(
+            sampleSheetDF['indexType'].str.contains(
+                "NuGEN Ovation SoLo RNA-Seq System"
+            )
+        ):
             log.info("NuGEN Ovation SoLo found for {}.".format(outputFolder))
             # Read 1
             mask.append(joinLis(seqRecipe['Read1']))
@@ -68,7 +74,9 @@ def detMask(seqRecipe, sampleSheetDF, outputFolder):
             if recipeP7-minP7 > 0:
                 mask.append("I{}U{}".format(minP7, recipeP7-minP7))
             else:
-                log.warning("NuGEN Ovation solo detected but Index read length == P7, no UMI!")
+                log.warning(
+                    "NuGEN Ovation solo Index read length == P7!"
+                )
                 mask.append("I{}".format(minP7))
             # Index 2
             if dualIx:
@@ -79,8 +87,11 @@ def detMask(seqRecipe, sampleSheetDF, outputFolder):
             # set UMI ops.
             convertOpts = ['CreateFastQForIndexReads,1,,', 'TrimUMI,0,,']
             return ";".join(mask), dualIx, PE, convertOpts
-        ####################### scATAC seq #######################
-        elif any(sampleSheetDF['indexType'].str.contains("ATAC-Seq single cell")):
+        # scATAC
+        elif any(sampleSheetDF['indexType'].str.contains(
+            "ATAC-Seq single cell"
+            )
+        ):
             log.info("scATAC seq found for {}".format(outputFolder))
             # Read 1
             mask.append(joinLis(seqRecipe['Read1']))
@@ -90,7 +101,9 @@ def detMask(seqRecipe, sampleSheetDF, outputFolder):
             if 'Index2' in seqRecipe:
                 mask.append("U{}".format(recipeP5))
             if dualIx:
-                log.warning("Indices detect in the sampleSheet. Mixed modalities not processed by default.")
+                log.warning(
+                    "P5 detected, Mixed modalities not processed by default."
+                )
             # Read 2
             if PE:
                 mask.append(joinLis(seqRecipe['Read2']))
@@ -98,8 +111,10 @@ def detMask(seqRecipe, sampleSheetDF, outputFolder):
                 log.warning("Single end sequencing.")
             convertOpts = []
             return ";".join(mask), dualIx, PE, convertOpts
-        else: #general case.
-            log.info("{} is not special. Setting default masks".format(outputFolder))
+        else:  # general case.
+            log.info(
+                "{} is not special. Setting default mask".format(outputFolder)
+            )
             # Read 1
             mask.append(joinLis(seqRecipe['Read1']))
             # Index 1 (sample barcode)
@@ -115,25 +130,28 @@ def detMask(seqRecipe, sampleSheetDF, outputFolder):
             convertOpts = []
             return ";".join(mask), dualIx, PE, convertOpts
 
+
 def prepConvert(flowcell, sampleSheet):
     log.warning("PreFQ module")
     log.info("determine masking")
     for outputFolder in sampleSheet.ssDic:
         sampleSheet.ssDic[outputFolder]['mask'], \
-        sampleSheet.ssDic[outputFolder]['dualIx'], \
+            sampleSheet.ssDic[outputFolder]['dualIx'], \
             sampleSheet.ssDic[outputFolder]['PE'], \
-        sampleSheet.ssDic[outputFolder]['convertOpts'] = detMask(
+            sampleSheet.ssDic[outputFolder]['convertOpts'] = detMask(
                 flowcell.seqRecipe,
                 sampleSheet.ssDic[outputFolder]['sampleSheet'],
                 outputFolder,
             )
         sampleSheet.ssDic[outputFolder]['mismatch'] = misMatcher(
             sampleSheet.ssDic[outputFolder]['sampleSheet']['index'],
-            sampleSheet.ssDic[outputFolder]['sampleSheet']['index2'] if 'index2' in sampleSheet.ssDic[outputFolder]['sampleSheet'] else pd.Series()
+            P5Seriesret(
+                sampleSheet.ssDic[outputFolder]['sampleSheet']
+            )
         )
     return sampleSheet
 
-#demux(sampleSheet.ssDic[outLane]['sampleSheet'], sampleSheet.ssDic[outLane]['mask'], outFolder)
+
 def writeDemuxSheet(demuxOut, ssDic, laneSplitStatus):
     demuxSheetLines = []
     # Header first.
@@ -141,9 +159,17 @@ def writeDemuxSheet(demuxOut, ssDic, laneSplitStatus):
     demuxSheetLines.append("FileFormatVersion,2,,")
     demuxSheetLines.append(",,,")
     demuxSheetLines.append("[BCLConvert_Settings],,,")
-    demuxSheetLines.append("BarcodeMismatchesIndex1,{},,".format(ssDic['mismatch']['BarcodeMismatchesIndex1']))
+    demuxSheetLines.append(
+        "BarcodeMismatchesIndex1,{},,".format(
+            ssDic['mismatch']['BarcodeMismatchesIndex1']
+        )
+    )
     if ssDic['dualIx']:
-        demuxSheetLines.append("BarcodeMismatchesIndex2,{},,".format(ssDic['mismatch']['BarcodeMismatchesIndex2']))
+        demuxSheetLines.append(
+            "BarcodeMismatchesIndex2,{},,".format(
+                ssDic['mismatch']['BarcodeMismatchesIndex2']
+            )
+        )
     demuxSheetLines.append("OverrideCycles,{},,".format(ssDic['mask']))
     if len(ssDic['convertOpts']) > 0:
         for opts in ssDic['convertOpts']:
@@ -152,23 +178,71 @@ def writeDemuxSheet(demuxOut, ssDic, laneSplitStatus):
     if ssDic['dualIx']:
         demuxSheetLines.append("[BCLConvert_Data],,,,,,")
         if laneSplitStatus:
-            demuxSheetLines.append("Lane,Sample_ID,index,index2,Sample_Project")
+            demuxSheetLines.append(
+                "Lane,Sample_ID,index,index2,Sample_Project"
+            )
             for index, row in ssDic['sampleSheet'].iterrows():
-                demuxSheetLines.append(joinLis([row['Lane'], row['Sample_ID'], row['index'], row['index2'], row['Sample_Project']], joinStr = ","))
+                demuxSheetLines.append(
+                    joinLis(
+                        [
+                            row['Lane'],
+                            row['Sample_ID'],
+                            row['index'],
+                            row['index2'],
+                            row['Sample_Project']
+                        ],
+                        joinStr=","
+                    )
+                )
         else:
-            demuxSheetLines.append("Sample_ID,index,index2,Sample_Project")
+            demuxSheetLines.append(
+                "Sample_ID,index,index2,Sample_Project"
+            )
             for index, row in ssDic['sampleSheet'].iterrows():
-                demuxSheetLines.append(joinLis([row['Sample_ID'], row['index'], row['index2'], row['Sample_Project']], joinStr = ","))
+                demuxSheetLines.append(
+                    joinLis(
+                        [
+                            row['Sample_ID'],
+                            row['index'],
+                            row['index2'],
+                            row['Sample_Project']
+                        ],
+                        joinStr=","
+                    )
+                )
     else:
         demuxSheetLines.append("[BCLConvert_Data],,,,,")
         if laneSplitStatus:
-            demuxSheetLines.append("Lane,Sample_ID,index,Sample_Project")
+            demuxSheetLines.append(
+                "Lane,Sample_ID,index,Sample_Project"
+            )
             for index, row in ssDic['sampleSheet'].iterrows():
-                demuxSheetLines.append(joinLis([row['Lane'], row['Sample_ID'], row['index'], row['Sample_Project']], joinStr=","))
+                demuxSheetLines.append(
+                    joinLis(
+                        [
+                            row['Lane'],
+                            row['Sample_ID'],
+                            row['index'],
+                            row['Sample_Project']
+                        ],
+                        joinStr=","
+                    )
+                )
         else:
-            demuxSheetLines.append("Sample_ID,index,Sample_Project")
+            demuxSheetLines.append(
+                "Sample_ID,index,Sample_Project"
+            )
             for index, row in ssDic['sampleSheet'].iterrows():
-                demuxSheetLines.append(joinLis([row['Sample_ID'], row['index'], row['Sample_Project']], joinStr=","))
+                demuxSheetLines.append(
+                    joinLis(
+                        [
+                            row['Sample_ID'],
+                            row['index'],
+                            row['Sample_Project']
+                        ],
+                        joinStr=","
+                    )
+                )
     with open(demuxOut, 'w') as f:
         for line in demuxSheetLines:
             f.write('{}\n'.format(line))
@@ -187,12 +261,18 @@ def demux(sampleSheet, flowcell, config):
             log.info("{} already exists. Moving on.".format(outputFolder))
         # Write the demuxSheet in the outputfolder
         demuxOut = os.path.join(outputFolder, "demuxSheet.csv")
-        # Create an escape for when the demuxSheet is already there (manual override)
+        # Don't remake if demuxSheet exist
         if not os.path.exists(demuxOut):
             log.info("Writing demuxSheet for {}".format(outLane))
-            writeDemuxSheet(demuxOut, sampleSheet.ssDic[outLane], sampleSheet.laneSplitStatus)
+            writeDemuxSheet(
+                demuxOut,
+                sampleSheet.ssDic[outLane],
+                sampleSheet.laneSplitStatus
+            )
         else:
-            log.info("demuxSheet for {} already exists. Not overwriting".format(outLane))
+            log.warning(
+                "demuxSheet for {} already exists.".format(outLane)
+            )
         # Run bcl-convert
         bclOpts = [
             config['software']['bclconvert'],
@@ -208,7 +288,7 @@ def demux(sampleSheet, flowcell, config):
         log.info(" ".join(bclOpts))
         bclRunner = Popen(
             bclOpts,
-            stdout = PIPE
+            stdout=PIPE
         )
         with bclRunner.stdout:
             bclConvPipeLogger(bclRunner.stdout)
@@ -218,4 +298,3 @@ def demux(sampleSheet, flowcell, config):
         else:
             log.critical("bclConvert exit {}".format(exitcode))
             sys.exit(1)
-        
