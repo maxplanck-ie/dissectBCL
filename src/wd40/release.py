5a565f1de48c64ef53dcbf57924abdacca7e557d
adRn-s <lims@omics.dev>
2024-01-26 14:30:19 +0100

    autoreformatted for flake8 rule

--- src/wd40/release.py
+++ src/wd40/release.py
@@ -8,22 +8,12 @@ from rich import print
 
 
 def fetchLatestSeqDir(pref, PI, postfix):
-    globStr = os.path.join(
-        pref,
-        PI,
-        postfix + '*'
-    )
+    globStr = os.path.join(pref, PI, postfix + "*")
     if len(glob.glob(globStr)) == 1:
         return glob.glob(globStr)[0]
     else:
         maxFolder = 0
-        for seqDir in glob.glob(
-            os.path.join(
-                pref,
-                PI,
-                postfix + '*'
-            )
-        ):
+        for seqDir in glob.glob(os.path.join(pref, PI, postfix + "*")):
             try:
                 seqInt = int(seqDir[-1])
             except ValueError:
@@ -31,17 +21,13 @@ def fetchLatestSeqDir(pref, PI, postfix):
                 continue
             if seqInt > maxFolder:
                 maxFolder = seqInt
-        return (os.path.join(
-            pref,
-            PI,
-            postfix + str(maxFolder)
-        ))
+        return os.path.join(pref, PI, postfix + str(maxFolder))
 
 
 def fetchFolders(flowcellPath, piList, prefix, postfix):
     institute_PIs = piList
     flowcellPath = os.path.abspath(flowcellPath)
-    FID = flowcellPath.split('/')[-1]
+    FID = flowcellPath.split("/")[-1]
     projDic = {}
     try:
         int(FID[:6])
@@ -50,33 +36,26 @@ def fetchFolders(flowcellPath, piList, prefix, postfix):
         sys.exit(
             "First 6 digits of flowcellpath don't convert to an int. Exiting."
         )
-    for projF in glob.glob(
-        os.path.join(
-            flowcellPath,
-            'Project_*'
-        )
-    ):
-        proj = projF.split('/')[-1]
+    for projF in glob.glob(os.path.join(flowcellPath, "Project_*")):
+        proj = projF.split("/")[-1]
         PI = proj.split("_")[-1].lower()
-        if PI == 'cabezas-wallscheid':
-            PI = 'cabezas'
+        if PI == "cabezas-wallscheid":
+            PI = "cabezas"
         if PI in institute_PIs:
             seqFolder = fetchLatestSeqDir(prefix, PI, postfix)
-            if os.path.exists(
-                os.path.join(seqFolder, FID)
-            ):
+            if os.path.exists(os.path.join(seqFolder, FID)):
                 projDic[proj] = [
-                    PI + 'grp',
+                    PI + "grp",
                     [
                         os.path.join(seqFolder, FID),
                         os.path.join(seqFolder, FID, proj),
-                        os.path.join(seqFolder, FID, 'FASTQC_' + proj),
+                        os.path.join(seqFolder, FID, "FASTQC_" + proj),
                         os.path.join(
                             seqFolder,
                             FID,
-                            'Analysis_' + proj.replace('Project_', '')
-                        )
-                    ]
+                            "Analysis_" + proj.replace("Project_", ""),
+                        ),
+                    ],
                 ]
             else:
                 print(
@@ -109,12 +88,8 @@ def release_folder(grp, lis):
     succes_fqc = release_rights(fastqcF, grp)
     if os.path.exists(analysisF):
         succes_analysis = release_rights(analysisF, grp)
-        return (
-            [succes_project, succes_fqc, succes_analysis]
-        )
-    return (
-        [succes_project, succes_fqc]
-    )
+        return [succes_project, succes_fqc, succes_analysis]
+    return [succes_project, succes_fqc]
 
 
 def release_rights(F, grp):
@@ -124,10 +99,7 @@ def release_rights(F, grp):
     for r, dirs, files in os.walk(F):
         for d in dirs:
             try:
-                os.chmod(
-                    os.path.join(r, d),
-                    0o750
-                )
+                os.chmod(os.path.join(r, d), 0o750)
                 changed += 1
             except PermissionError:
                 print("Permission error for {}".format(d))
@@ -138,10 +110,7 @@ def release_rights(F, grp):
                 if grp != Path(fil).group():
                     grouperror = True
                 try:
-                    os.chmod(
-                        fil,
-                        0o750
-                    )
+                    os.chmod(fil, 0o750)
                     changed += 1
                 except PermissionError:
                     print("Permission error for {}".format(f))
@@ -153,76 +122,77 @@ def release_rights(F, grp):
                 F
             )
         )
-    return (successRate)
+    return successRate
 
 
-def rel(flowcellPath, piList, prefix, postfix, parkourURL, parkourAuth, parkourCert, fexBool, fromAddress):
-    projDic = fetchFolders(
-        flowcellPath,
-        piList,
-        prefix,
-        postfix
-    )
+def rel(
+    flowcellPath,
+    piList,
+    prefix,
+    postfix,
+    parkourURL,
+    parkourAuth,
+    parkourCert,
+    fexBool,
+    fromAddress,
+):
+    projDic = fetchFolders(flowcellPath, piList, prefix, postfix)
     print("Print number of changed/(changed+unchanged)!")
     for proj in projDic:
-        '''
+        """
         every projDic[proj] is a nested list of:
         [grp, [flowcell, project, fastqc]]
-        '''
+        """
         successes = release_folder(projDic[proj][0], projDic[proj][1])
         if len(successes) == 2:
             print(
                 "[green]Project[/green] {},{} proj,{} fqc".format(
-                    proj,
-                    successes[0],
-                    successes[1]
+                    proj, successes[0], successes[1]
                 )
             )
         else:
             print(
                 "[green]Project[/green] {},{} proj,{} fqc,{} analysis".format(
-                    proj,
-                    successes[0],
-                    successes[1],
-                    successes[2]
+                    proj, successes[0], successes[1], successes[2]
                 )
             )
-        projectPath = projDic[proj][1][1].split('/')[-1]
-        PI = projectPath.split('_')[-1].lower().replace(
-            "cabezas-wallscheid", "cabezas"
+        projectPath = projDic[proj][1][1].split("/")[-1]
+        PI = (
+            projectPath.split("_")[-1]
+            .lower()
+            .replace("cabezas-wallscheid", "cabezas")
         )
         d = None
         if PI in piList:
             d = {
-                    "data": projDic[proj][1][1],
-                    "metadata": projDic[proj][1][1] + '/multiqc_report.html'
-                }
+                "data": projDic[proj][1][1],
+                "metadata": projDic[proj][1][1] + "/multiqc_report.html",
+            }
         elif fexBool:
-            fexList = check_output(
-                [
-                    'fexsend',
-                    '-l',
-                    fromAddress
-                ]
-            ).decode("utf-8").replace("\n", " ").split(' ')
-            tar_lane, tar_proj = projDic[proj][1][1].split('/')[-2:]
-            # e.g. ['230731_M01358_0029_000000000-KYBFN_lanes_1', 'Project_2852_Trancoso_Boehm']
-            tarBall =  tar_lane + '_' + tar_proj + '.tar'
+            fexList = (
+                check_output(["fexsend", "-l", fromAddress])
+                .decode("utf-8")
+                .replace("\n", " ")
+                .split(" ")
+            )
+            tar_lane, tar_proj = projDic[proj][1][1].split("/")[-2:]
+            tarBall = tar_lane + "_" + tar_proj + ".tar"
             if tarBall in fexList:
-                d = {
-                        "data": tarBall,
-                        "metadata": None
-                    }
+                d = {"data": tarBall, "metadata": None}
             else:
-                print("fexLink: ", tarBall, " not found!") 
+                print("fexLink: ", tarBall, " not found!")
         if d:
-            print("Adding filepaths to Parkour2:",
+            print(
+                "Adding filepaths to Parkour2:",
                 requests.post(
-                    parkourURL + '/api/requests/' + proj.split('_')[1] + '/put_filepaths/',
-                    auth = parkourAuth,
-                    data = d,
-                    verify = parkourCert
-                    )
+                    parkourURL
+                    + "/api/requests/"
+                    + proj.split("_")[1]
+                    + "/put_filepaths/",
+                    auth=parkourAuth,
+                    data=d,
+                    verify=parkourCert,
+                ),
             )  # print the returned answer from the API
         else:
             print("Warning: Unrecognized PI or fexBool was False")
