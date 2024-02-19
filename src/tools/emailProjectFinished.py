85bd918e3dbc407cf28122cf78ed9e9cf2ecc20d
adRn-s <lims@omics.dev>
2024-02-19 14:17:44 +0100

    compound surnames, support how our IT handles them

--- src/tools/emailProjectFinished.py
+++ src/tools/emailProjectFinished.py
@@ -32,7 +32,7 @@ def getProjectIDs(projects, config):
         # Sanity check
         assert (p.startswith("Project_"))
         IDs.append(p.split("_")[1])
-        PI = p.split("_")[-1].lower()
+        PI = p.split("_")[-1].split("-")[0].lower()  # compound surnames use minus, we use 1st only.
     # Get the actual sequencing_data dir
     # Assume if multiple projects are given, they all in the same flowcell.
     flowcell = getFlowCell()
