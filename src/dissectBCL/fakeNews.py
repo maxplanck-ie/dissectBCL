b26eb4279fb7b00976ad12a45aadd6cd12082cf3
adRn-s <salatino@omics.dev>
2024-12-11 17:08:53 +0100

    potential fix, we need to test

--- src/dissectBCL/fakeNews.py
+++ src/dissectBCL/fakeNews.py
@@ -442,9 +442,9 @@ def gatherFinalMetrics(outLane, flowcell):
         # samples with 0 reads still make an empty report.
         # hence the try / except.
         # 'mouse (GRCm39)' -> 'mouse'
-        parkourOrg = str(  # To string since NA is a float
-                ssdf[ssdf["Sample_ID"] == sampleID]['Organism'].values[0][0]
-            ).split(' ')[0]
+        parkourOrg = str(
+            ssdf[ssdf["Sample_ID"] == sampleID]['Organism'].values[0][1]
+        )
         try:
             screenDF = pd.read_csv(
                 screen, sep='\t', header=None
