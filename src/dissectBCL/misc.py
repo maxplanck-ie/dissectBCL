5096bb74706c6e1f10a888ed1f1de98a58732258
katsikora <sikora@ie-freiburg.mpg.de>
2025-07-28 15:11:24 +0200

    no underscore

--- src/dissectBCL/misc.py
+++ src/dissectBCL/misc.py
@@ -383,7 +383,7 @@ def fetchLatestSeqDir(config, PI):
     if seqDirNum == 0:
         return Path(PIpath, 'sequencing_data')
     else:
-        return Path(PIpath) / 'sequencing_data_{}'.format(str(seqDirNum))
+        return Path(PIpath) / 'sequencing_data{}'.format(str(seqDirNum))
 
 
 def umlautDestroyer(germanWord):
